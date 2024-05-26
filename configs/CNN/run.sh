06f1ac3b85f4ea025a768ba9ebfd4e13190342af
olokevin <yequan_zhao@ucsb.edu>
2024-05-26 12:56:26 -0700

    0526

--- configs/CNN/run.sh
+++ configs/CNN/run.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0 python CNN/tinytl_fgvc_train.py configs/CNN/test.yaml --e
 
 ### mcunet pretrain
 CUDA_VISIBLE_DEVICES=1 python CNN/tinytl_fgvc_train.py configs/CNN/pretrain.yaml
-CUDA_VISIBLE_DEVICES=1 nohup python CNN/tinytl_fgvc_train.py configs/CNN/pretrain.yaml >/dev/null 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python CNN/tinytl_fgvc_train.py configs/CNN/pretrain.yaml >/dev/null 2>&1 &
 
 ### mcunet transfer
 CUDA_VISIBLE_DEVICES=1 python CNN/tinytl_fgvc_train.py configs/CNN/transfer.yaml
